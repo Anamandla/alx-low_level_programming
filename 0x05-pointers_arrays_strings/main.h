b3e6bcf0faa2b315e2782cbaaef171c54fe89496
Anamandla Zweni <anamandla38@gmail.com>
2022-09-20 19:20:09 -0700

    idk

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,10 @@
-#ifndef _MAIN_H_
+#ifndef MAIN_H
 
-#define _MAIN_H_
+#define MAIN_H
 
 
 
-void _putchar (char c); 
+int _putchar(char c);
 
 void reset_to_98(int *n);
 
@@ -26,6 +26,8 @@ void print_array(int *a, int n);
 
 char *_strcpy(char *dest, char *src);
 
+int _atoi(char *s);
 
 
-#endif 
+
+#endif /* MAIN_H */
