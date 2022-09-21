4d18a917f2d70299c4ff882705528f13310e8a8c
Anamandla Zweni <anamandla38@gmail.com>
2022-09-20 19:10:32 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,18 +1,11 @@
 #include "main.h"
 
-/**
- *
- * reset_to_98 -a function that takes a pointer to an int
- *
- * @n: puntero
- *
- * Return: (0)
- *
- */
+#include <stdio.h>
 
 void reset_to_98(int *n)
 
 {
+
 	*n = 98;
 
 }
